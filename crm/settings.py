43c64f54b8e87d29e49418057d57e6d046e2d256
Abuki <abubeker.jemal03@gmail.com>
2024-05-02 15:53:41 +0100

    vercel deployment

--- crm/settings.py
+++ crm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3p3%bhz(2=1r*asah344xsx-hei$t6pvq%73tdrcy!i&y2_-b3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
