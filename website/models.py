f428725c904f06b5fc878c694f69feabab40d119
AbubekerJ <110613188+AbubekerJ@users.noreply.github.com>
2024-05-06 09:31:59 +0100

    Update models.py

--- website/models.py
+++ website/models.py
@@ -7,7 +7,7 @@ class Customer(models.Model):
     
 
      first_name =models.CharField( max_length=50)
-     last_name =models.CharField( max_length=50 blank=True, null=True)
+     last_name =models.CharField( max_length=50 )
      email =models.EmailField( max_length=254, blank=True, null=True)
      phone_numer= models.CharField(max_length=20)
      city =models.CharField( max_length=50)
